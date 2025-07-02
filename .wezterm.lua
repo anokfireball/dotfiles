a77229280e252abc000e26684c7a8b2331b36674
Fabian Koller <github@kthxbye.cyou>
2025-07-02 10:58:59 +0200

    update wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,4 +1,15 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
+
+local target_os = wezterm.target_triple
 local config = wezterm.config_builder()
-config.color_scheme = 'Dracula'
+
+if target_os:find("apple") then
+	config.freetype_load_target = "HorizontalLcd"
+elseif target_os:find("windows") then
+	config.default_prog = { "wsl.exe", "--cd", "~" }
+end
+config.font_size = 14
+config.color_scheme = "Dracula"
+config.hide_tab_bar_if_only_one_tab = true
+
 return config
