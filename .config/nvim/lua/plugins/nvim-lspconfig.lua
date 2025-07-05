ef873fe182dec054f4be78b23188a5acf3201844
Fabian Koller <github@kthxbye.cyou>
2025-07-05 10:47:46 +0200

    nvim: split out plugin setup into dedicated subdir

---
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -0,0 +1,13 @@
+return {
+	"neovim/nvim-lspconfig",
+	dependencies = {
+		{ "mason-org/mason.nvim", opts = {} },
+		"mason-org/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		{ "j-hui/fidget.nvim", opts = {} },
+		"saghen/blink.cmp",
+	},
+	config = function()
+		-- ...existing LSP config from init.lua...
+	end,
+}
