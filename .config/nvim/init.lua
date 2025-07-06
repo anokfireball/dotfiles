32930c2176f9eaab2d5ff2bc13fa9443b4d1ccc5
Fabian Koller <github@kthxbye.cyou>
2025-07-06 17:33:00 +0200

    nvim: more subliminal which-key configuration

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -180,5 +180,6 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 
 require("config.lazy")
 
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -1,46 +1,27 @@
 return {
 	"folke/which-key.nvim",
-	lazy = true,
-	event = "VimEnter",
+	dependencies = {
+		{ "nvim-tree/nvim-web-devicons" },
+	},
+	config = function(ctx)
+		require("which-key").setup(ctx.opts)
+		vim.api.nvim_set_hl(0, "WhichKeyBorder", { link = "Normal" })
+		vim.api.nvim_set_hl(0, "WhichKeyNormal", { link = "Normal" })
+	end,
 	opts = {
-		delay = 0,
-		icons = {
-			mappings = vim.g.have_nerd_font,
-			keys = vim.g.have_nerd_font and {} or {
-				Up = "<Up> ",
-				Down = "<Down> ",
-				Left = "<Left> ",
-				Right = "<Right> ",
-				C = "<C-…> ",
-				M = "<M-…> ",
-				D = "<D-…> ",
-				S = "<S-…> ",
-				CR = "<CR> ",
-				Esc = "<Esc> ",
-				ScrollWheelDown = "<ScrollWheelDown> ",
-				ScrollWheelUp = "<ScrollWheelUp> ",
-				NL = "<NL> ",
-				BS = "<BS> ",
-				Space = "<Space> ",
-				Tab = "<Tab> ",
-				F1 = "<F1>",
-				F2 = "<F2>",
-				F3 = "<F3>",
-				F4 = "<F4>",
-				F5 = "<F5>",
-				F6 = "<F6>",
-				F7 = "<F7>",
-				F8 = "<F8>",
-				F9 = "<F9>",
-				F10 = "<F10>",
-				F11 = "<F11>",
-				F12 = "<F12>",
-			},
-		},
+		preset = "helix",
+		delay = 1000,
 		spec = {
-			{ "<leader>s", group = "[S]earch" },
-			{ "<leader>t", group = "[T]oggle" },
-			{ "<leader>h", group = "Git [H]unk", mode = { "n", "v" } },
+			{ "<leader>", group = "leader" },
+			{ "<leader>f", group = "[f]ind" },
+		},
+		plugins = {
+			spelling = { enabled = false },
+		},
+		win = {
+			title = false,
+			padding = { 1, 2 },
+			wo = { winblend = 0 },
 		},
 	},
 }
