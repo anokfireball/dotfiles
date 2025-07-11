ae80f8cac004036d2e89acf1a8cb5875914eef8c
Fabian Koller <github@kthxbye.cyou>
2025-07-11 09:03:43 +0200

    tmux: pane navigation via META+hjkl

--- .wezterm.lua
+++ .wezterm.lua
@@ -6,10 +6,12 @@ local config = wezterm.config_builder()
 if target_os:find("apple") then
 	config.font_size = 14
 	config.freetype_load_target = "HorizontalLcd"
-    -- consistent ctrl behaviour between both systems
 	config.keys = {
+		-- consistent ctrl behaviour between both systems
 		{ key = "c", mods = "SUPER", action = wezterm.action.DisableDefaultAssignment },
 		{ key = "v", mods = "SUPER", action = wezterm.action.DisableDefaultAssignment },
+		-- no tabs on the emulator level
+		{ key = "t", mods = "SUPER", action = wezterm.action.DisableDefaultAssignment },
 	}
 elseif target_os:find("windows") then
 	config.font_size = 11
@@ -22,15 +24,14 @@ elseif target_os:find("windows") then
 		},
 	}
 	config.default_domain = "WSL:Debian"
+	config.keys = {
+		-- no tabs on the emulator level
+		{ key = "t", mods = "CTRL|SHIFT", action = wezterm.action.DisableDefaultAssignment },
+	}
 end
 config.animation_fps = 144
 config.color_scheme = "Dracula (Official)"
 config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = "RESIZE"
-
-config.keys = {
-	{ key = "c", mods = "SUPER", action = wezterm.action.DisableDefaultAssignment },
-	{ key = "v", mods = "SUPER", action = wezterm.action.DisableDefaultAssignment },
-}
 
 return config
