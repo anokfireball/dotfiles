e88458c961c992f00dd2f9d84080edaad6d5dbc0
Fabian Koller <github@kthxbye.cyou>
2025-07-31 09:53:18 +0200

    nvim: replace abandoned and buggy shade with vimade

---
+++ .config/nvim/lua/plugins/vimade.lua
@@ -0,0 +1,9 @@
+return {
+	"tadaa/vimade",
+	opts = {
+		recipe = { "default", { animate = true } },
+		ncmode = "windows",
+		fadelevel = 0.5,
+		tint = { bg = { rgb = { 0, 0, 0 }, intensity = 0.3 } },
+	},
+}
