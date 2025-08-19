c823985a6e3e121c721e5407179dfd225fe335ff
Fabian Koller <github@kthxbye.cyou>
2025-08-19 09:43:11 +0200

    nvim: tweaks to telescope keymaps

--- .config/zsh/modules/30-keymap.zsh
+++ .config/zsh/modules/30-keymap.zsh
@@ -3,3 +3,7 @@ bindkey -v
 # Use bash-like word boundaries
 autoload -Uz select-word-style
 select-word-style bash
+
+# Custom keybindings
+bindkey "^[[1;2C" forward-word   # Shift+Right Arrow
+bindkey "^[[1;2D" backward-word  # Shift+Left Arrow
