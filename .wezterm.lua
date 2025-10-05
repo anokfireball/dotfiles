fe985aedebe8c1b29c3ad2f111d6e494aed6c558
Fabian Koller <github@kthxbye.cyou>
2025-10-05 13:19:51 +0200

    wezterm: use JetBrainsMono NF explicitly

--- .wezterm.lua
+++ .wezterm.lua
@@ -5,7 +5,13 @@ local target_os = wezterm.target_triple
 ---@type Config
 local config = wezterm.config_builder()
 
+local base_mono = "JetBrainsMono Nerd Font"
+
 if target_os:find("apple") then
+	config.font = wezterm.font_with_fallback({
+		base_mono,
+		{ family = "Apple Color Emoji", assume_emoji_presentation = true },
+	})
 	config.font_size = 14
 	config.freetype_load_target = "HorizontalLcd"
 	config.keys = {
@@ -16,6 +22,10 @@ if target_os:find("apple") then
 		{ key = "t", mods = "SUPER", action = wezterm.action.DisableDefaultAssignment },
 	}
 elseif target_os:find("windows") then
+	config.font = wezterm.font_with_fallback({
+		base_mono,
+		{ family = "Segoe UI Emoji", assume_emoji_presentation = true },
+	})
 	config.font_size = 11
 	config.wsl_domains = {
 		{
@@ -31,6 +41,7 @@ elseif target_os:find("windows") then
 		{ key = "t", mods = "CTRL|SHIFT", action = wezterm.action.DisableDefaultAssignment },
 	}
 end
+config.line_height = 0.9
 config.animation_fps = 144
 config.color_scheme = "Dracula (Official)"
 
@@ -38,5 +49,6 @@ config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = "RESIZE"
 config.scrollback_lines = 250 -- 4K @ 16 pixels per line = ~135 lines
 config.enable_scroll_bar = false
+config.swallow_mouse_click_on_window_focus = true
 
 return config
